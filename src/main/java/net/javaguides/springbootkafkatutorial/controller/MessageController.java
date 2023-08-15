501b6732425c20f7221b47e6bdf3b073c5b77f29
Hp <Hp@192.168.1.7>
2023-08-15 16:54:14 +0530

    kafka_tutorial _project

---
+++ src/main/java/net/javaguides/springbootkafkatutorial/controller/MessageController.java
@@ -0,0 +1,37 @@
+package net.javaguides.springbootkafkatutorial.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.javaguides.springbootkafkatutorial.kafka.KafkaProducer;
+
+@RestController
+@RequestMapping
+
+public class MessageController {
+	
+	private KafkaProducer kafkaProducer;
+	
+	public MessageController(KafkaProducer kafkaProducer) {
+		this.kafkaProducer=kafkaProducer;
+	}
+	
+	//http:localhost:8080/api/v1/kafka/publish?message=hello world
+	@GetMapping("/publish")
+	public ResponseEntity<String> publish(@RequestParam("message") String message){
+		kafkaProducer.sendMessage(message);
+		return ResponseEntity.ok("Message sent to topic");
+		
+	}
+	
+	@GetMapping("/hello")
+	public ResponseEntity<String> publish2(){
+		//kafkaProducer.sendMessage(message);
+		return ResponseEntity.ok("Message sent to topic");
+		
+	}
+
+}
